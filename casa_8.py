90cf8fb8a65e87e26100e3eda53d8cf78c8a699e
rdeager14 <rdeager@miners.utep.edu>
2024-02-13 19:40:56 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,14 @@
+import pandas as pd
+
+
+def iterate_using_apply(df):
+    def process_row(row):
+        return row['Column1'] + row['Column2']  
+
+    result_series = df.apply(process_row, axis=1)
+    print(result_series) 
+
+df = pd.read_csv('big-mac-full-index.csv')
+
+
 
